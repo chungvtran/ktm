d6de8d07710c6a38edb51275b7326b1f03862491
hoanghta <hoanghta@kms-technology.com>
2020-03-20 08:35:39 +0700

    Base repository

---
+++ BE/Infrastructure/KMS.Product.Ktm.Repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace KMS.Product.Ktm.Repository.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        void Save();
+    }
+}
---
+++ BE/Infrastructure/KMS.Product.Ktm.Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using KMS.Product.Ktm.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace KMS.Product.Ktm.Repository.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly KtmContext _context;
+
+        public BaseRepository(KtmContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
